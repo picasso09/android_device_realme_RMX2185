52992eb1d75c3ed375a79a53a6b37a279e8135de
picasso09 <sayazaki282901@gmail.com>
2024-03-06 13:58:49 +0000

    RMX2185: add realme part

--- aosp_RMX2185.mk
+++ aosp_RMX2185.mk
@@ -1,18 +1,28 @@
 #
-# Copyright (C) 2018-2022 ArrowOS
+# Copyright (C) 2020 Android Open Source Project
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 # Inherit from device makefile
 $(call inherit-product, device/realme/RMX2185/device.mk)
 
-# Inherit some common ProjectElixir stuff.
+# Inherit some common NusantaraProject stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
@@ -22,24 +32,11 @@ PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX2185
 PRODUCT_MANUFACTURER := realme
 
-PRODUCT_SYSTEM_MODEL := RMX2185
-PRODUCT_SYSTEM_NAME := RMX2185
-PRODUCT_SYSTEM_DEVICE := RMX2185
-
-# ProjectElixir additions
+# aospFest additions
 IS_PHONE := true
 AOSP_BUILD_TYPE := UNOFFICIAL
-BUILD_HOSTNAME := DV-WORK
+BUILD_HOSTNAME := aospteam
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES := 720
-TARGET_SUPPORTS_GOOGLE_RECORDER := false
-TARGET_INCLUDE_STOCK_ARCORE := false
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
-TARGET_SUPPORTS_CALL_RECORDING := false
-
-# Build info
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
-    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
