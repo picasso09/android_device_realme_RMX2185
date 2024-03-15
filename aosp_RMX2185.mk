9ab9185d6f6b7634ddbd6fe31366090ac1ffafe9
picasso09 <119201284+picasso09@users.noreply.github.com>
2024-03-15 18:38:25 +0000

    RMX2185: pixel stuf
    Signed-off-by: picasso09 <119201284+picasso09@users.noreply.github.com>

--- aosp_RMX2185.mk
+++ aosp_RMX2185.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile
 $(call inherit-product, device/realme/RMX2185/device.mk)
 
-# Inherit some common NusantaraProject stuff.
+# Inherit some common PixelExperience stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
@@ -32,11 +32,12 @@ PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX2185
 PRODUCT_MANUFACTURER := realme
 
-# aospFest additions
+# Pixel additions
 IS_PHONE := true
 AOSP_BUILD_TYPE := UNOFFICIAL
 BUILD_HOSTNAME := aospteam
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES := 720
+TARGET_INCLUDE_STOCK_ARCORE := false
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
