d4e51b3bd31dcb88c423d64c0733082e8b596bfd
picasso09 <sayazaki282901@gmail.com>
2024-03-19 02:32:39 +0000

    RMX2185: pixel bringup UI2

--- extract-files.sh
+++ extract-files.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2018-2020 The LineageOS Project
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 set -e
@@ -15,7 +24,7 @@ VENDOR=realme
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-ANDROID_ROOT="${MY_DIR}/../../.."
+ANDROID_ROOT="${MY_DIR}"/../../..
 
 HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
@@ -26,9 +35,6 @@ source "${HELPER}"
 
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
-
-KANG=
-SECTION=
 
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
@@ -53,65 +59,10 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
-function blob_fixup {
-    case "$1" in
-        lib/libsink.so)
-            grep -q "libshim_vtservice.so" "${2}" || patchelf --add-needed "libshim_vtservice.so" "${2}"
-            ;;
-        vendor/lib*/hw/audio.primary.mt6765.so)
-            grep -q "libmedia_helper-v30.so" "${2}" || patchelf --replace-needed "libmedia_helper.so" "libmedia_helper-v30.so" "${2}"
-            grep -q "libalsautils-v30.so" "${2}" || patchelf --replace-needed "libalsautils.so" "libalsautils-v30.so" "${2}"
-            ;;
-        vendor/lib*/hw/audio.usb.mt6765.so)
-            grep -q "libalsautils-v30.so" "${2}" || patchelf --replace-needed "libalsautils.so" "libalsautils-v30.so" "${2}"
-            ;; 
-        vendor/bin/hw/hostapd)
-            ;&
-        vendor/bin/hw/wpa_supplicant)
-            grep -q "libcompiler_rt.so" "${2}" || patchelf --add-needed "libcompiler_rt.so" ${2}
-            ;;
-        vendor/bin/hw/camerasloganserver)
-            ;&
-        vendor/lib/libmtkcam_stdutils.so)
-            ;&
-        vendor/lib64/libmtkcam_stdutils.so)
-            grep -q "libutils-v29.so" "${2}" || patchelf --replace-needed "libutils.so" "libutils-v29.so" "${2}"
-            ;;
-        vendor/bin/hw/camerahalserver)
-            grep -q "libutils-v30.so" "${2}" || patchelf --replace-needed "libutils.so" "libutils-v30.so" "${2}"
-            ;;
-        vendor/lib64/hw/android.hardware.camera.provider@2.4-impl-mediatek.so)
-            grep -q libshim_camera_metadata.so "$2" || patchelf --add-needed libshim_camera_metadata.so "$2"
-            ;;
-        vendor/etc/init/android.hardware.bluetooth@1.0-service-mediatek.rc)
-            sed -i '/vts/Q' "$2"
-            ;;
-        vendor/etc/init/vendor.oppo.hardware.biometrics.fingerprint@2.1-service.rc)
-            sed -i '/cpuset/Q' "$2"
-            ;;
-        vendor/lib64/hw/vendor.mediatek.hardware.pq@2.3-impl.so)
-            grep -q "libutils-v30.so" "${2}" || patchelf --replace-needed "libutils.so" "libutils-v30.so" "${2}"
-            ;;
-        vendor/lib/libMtkOmxVdecEx.so)
-            grep -q "libui-v32.so" "${2}" || patchelf --replace-needed "libui.so" "libui-v32.so" "$2"
-            ;;
-            vendor/etc/init/android.hardware.drm@1.2-service.widevine.rc)
-            ;&
-        vendor/etc/init/camerasloganserver.rc)
-            sed -i 's|writepid /dev/cpuset/foreground/tasks|task_profiles ProcessCapacityHigh|g' "$2"
-            ;;
-        vendor/etc/init/camerahalserver.rc)
-            sed -i 's|writepid /dev/cpuset/camera-daemon/tasks /dev/stune/top-app/tasks|task_profiles CameraServiceCapacity MaxPerformance|g' "$2"
-            ;;
-        lib/libshowlogo.so)
-            grep -q "libshim_showlogo.so" "${2}" || patchelf --add-needed "libshim_showlogo.so" "${2}"
-            ;;
-    esac
-}
-
 # Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
 
-extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
+        "${KANG}" --section "${SECTION}"
 
 "${MY_DIR}/setup-makefiles.sh"
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2018-2020 The LineageOS Project
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 set -e
 
 DEVICE=RMX2185
 VENDOR=realme
+INITIAL_COPYRIGHT_YEAR=2020
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
@@ -27,9 +37,10 @@ source "${HELPER}"
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 
-# Warning headers and guards
-write_headers
+# Copyright headers and guards
+write_headers "${DEVICE}"
 
+# The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
 # Finish
