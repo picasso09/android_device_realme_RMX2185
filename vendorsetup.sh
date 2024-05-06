328bd26eed7ae9aa3ef6d26a123ca0f8a907c9cc
picasso09 <officialzki2006@gmail.com>
2024-05-06 22:42:33 +0700

    RMX2185: add more sepolicy update

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,8 @@
 echo "Cloning Dependencies"
 #git config --global credential.helper cache
-git clone https://github.com/techyminati/proprietary_vendor_realmeme_RMX2185 -b android-14.0 vendor/realme/RMX2185
+git clone https://github.com/realme-mediatek-dev/proprietary_vendor_realme_RMX2185 -b android-14.0 vendor/realme/RMX2185
 #git clone https://github.com/techyminati/android_packages_apps_RealmeParts -b android-13.0 packages/apps/RealmeParts
-git clone --depth=1 https://github.com/picasso09/android_kernel_RMX2189R -b c05-upstream
+git clone --depth=1 https://github.com/picasso09/android_kernel_RMX2189R -b c05
 rm -rf prebuilts/clang/host/linux-x86/proton-clang
 git clone --depth=1 https://github.com/picasso09/proton-clang prebuilts/clang/host/linux-x86/proton-clang
 # rm -rf packages/apps/FMRadio
