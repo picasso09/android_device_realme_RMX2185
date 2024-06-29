f2a43cbedf2ab8cbe5d7785be38aa49b71b9ad28
picasso09 <sayazaki282901@gmail.com>
2024-06-29 21:02:27 +0700

    RMX2185: vendorsetup: change repository target
    Signed-off-by: picasso09

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 echo 'Device Setup'
 git clone --depth=1 https://github.com/picasso09/android_vendor_realme_RMX2185 -b lineage-21-ui2 vendor/realme/RMX2185
 git clone --depth=1 https://github.com/picasso09/android_vendor_realme_RMX2185-ims vendor/realme/RMX2185-ims
-git clone --depth=1 https://github.com/picasso09/android_kernel_RMX2189R kernel/realme/RMX2185
+git clone --depth=1 https://github.com/picasso09/android_kernel_RMX2185 kernel/realme/RMX2185
 git clone --depth=1 https://github.com/picasso09/proton-clang prebuilts/clang/host/linux-x86/proton-clang
 git clone --depth=1 https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
 echo 'Done go to cook'
